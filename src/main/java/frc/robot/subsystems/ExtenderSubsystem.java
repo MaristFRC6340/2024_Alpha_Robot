fba6cca446bc831b8610e24e3d563e533e1dd8e4
nebomusic <michaudc@marist.com>
2024-02-20 08:03:14 -0500

    Russels draft code

--- src/main/java/frc/robot/subsystems/ExtenderSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExtenderSubsystem.java
@@ -4,14 +4,51 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import frc.robot.Constants.ExtenderConstants;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExtenderSubsystem extends SubsystemBase {
+
+  private CANSparkMax armExtender;
+
+  private RelativeEncoder encoder;
+
+  private SparkPIDController extenderPID;
   /** Creates a new ArmSubsystem. */
-  public ExtenderSubsystem() {}
+  public ExtenderSubsystem() {
+    armExtender = new CANSparkMax(ExtenderConstants.kExtenderID, MotorType.kBrushless);
+
+    armExtender.setSmartCurrentLimit(40);
+
+    armExtender.setIdleMode(IdleMode.kBrake);
+
+    encoder = armExtender.getEncoder();
+
+    extenderPID = armExtender.getPIDController();
+    extenderPID.setP(ExtenderConstants.kP);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+
+  //Methods
+
+  public void setExtenderPower(double power) {
+    armExtender.set(power);
+  }
+
+  public void stop() {
+    armExtender.set(0);
+  }
+
+  public void goToPosition()
 }
